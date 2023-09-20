9aa0a2df2a5c0569eb8f63ff6232dc2b89d89ccc
Victor Villazon <victorzn23@gmail.com>
2023-09-19 22:56:56 -0500

    manipulado elementos html desde js

--- app.js
+++ app.js
@@ -3,10 +3,14 @@
 const redColor = document.querySelector('.red')
 // const cardButton = document.querySelector('#button')
 const cardButton = document.getElementById('button')
+const cardImage = document.querySelector('.card__image img')
 
 console.log(redColor)
+
+// Agregando el evento addEventListener
 
 redColor.addEventListener('click', () => {
   console.log('Elegiste el color rojo')
   cardButton.style.backgroundColor = 'red'
+  cardImage.src = './assets/mercedez_red.jpg'
 })
