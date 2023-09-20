e0c9e7c4288ebde03d2f0c5ab613d508db2d872c
Victor Villazon <victorzn23@gmail.com>
2023-09-19 23:24:00 -0500

    terminando el ejercicio con la librería confetti-canvas

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const redColor = document.querySelector('.red')
 const grayColor = document.querySelector('.gray')
 // const cardButton = document.querySelector('#button')
 const cardButton = document.getElementById('button')
+const cardButtonFeedback = document.getElementById('feedback')
 const cardImage = document.querySelector('.card__image img')
+const cardDescription = document.querySelector('.card__description')
+const cardPrice = document.querySelector('.card__price')
 
 console.log(redColor)
 
@@ -19,4 +22,37 @@ redColor.addEventListener('click', () => {
 grayColor.addEventListener('click', () => {
   cardButton.style.backgroundColor = 'gray'
   cardImage.src = './assets/mercedez_gray.jpg'
+})
+
+// TODO: agrgar el evento click al boton black
+
+// Crear etiqueta h3, agregarle una clase, un texto y lo vamos a incrustar en el html desde JS
+
+// appendChild
+const etiquetaH3 = document.createElement('h3')
+etiquetaH3.className = 'tag'
+etiquetaH3.textContent = 'Card'
+// etiquetaH3.innerHTML = '<u>Card</u>'
+// cardDescription.appendChild(etiquetaH3)
+
+// insertBefore
+const cardDescriptionH2 = document.querySelector('h2')
+cardDescription.insertBefore(etiquetaH3, cardDescriptionH2)
+
+// Cambiando el contenido del price
+cardPrice.innerHTML = '$200,000.00'
+
+cardButton.addEventListener('click', function () {
+  cardButton.style.display = 'none'
+  cardButtonFeedback.style.display = 'block'
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 1 }
+  })
+})
+
+cardButtonFeedback.addEventListener('click', function () {
+  cardButton.style.display = 'block'
+  cardButtonFeedback.style.display = 'none'
 })
