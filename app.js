38de9dfea8cfaa5ac07425a76eee89ffb0d49ea0
Victor Villazon <victorzn23@gmail.com>
2023-09-19 22:59:35 -0500

    agregando interacción al boton gray

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // console.log('Hola Card!')
 
 const redColor = document.querySelector('.red')
+const grayColor = document.querySelector('.gray')
 // const cardButton = document.querySelector('#button')
 const cardButton = document.getElementById('button')
 const cardImage = document.querySelector('.card__image img')
@@ -13,4 +14,9 @@ redColor.addEventListener('click', () => {
   console.log('Elegiste el color rojo')
   cardButton.style.backgroundColor = 'red'
   cardImage.src = './assets/mercedez_red.jpg'
+})
+
+grayColor.addEventListener('click', () => {
+  cardButton.style.backgroundColor = 'gray'
+  cardImage.src = './assets/mercedez_gray.jpg'
 })
